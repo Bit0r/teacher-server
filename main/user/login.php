1474f0e6ea535bf1c391ca6c59fc7cc03c623aac
Bit0r <nie_wang@outlook.com>
2020-06-12 20:58:27 +0800

    📦 用户登陆

---
+++ main/user/login.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../../lib/teacher-funcs.php';
+
+$user = json_input();
+
+json_header();
+
+try {
+    $db = connect_teacher();
+
+    $query =
+        'SELECT password_hash, role_type
+        FROM users
+        WHERE username = ?';
+    $stmt = $db->prepare($query);
+    $stmt->execute([$user['username']]);
+    $result = $stmt->fetch();
+
+    # 验证账号密码
+    if ($stmt->rowCount() == 0) {
+        throw new Exception("账号错误", 1);
+    } elseif (!password_verify($user['password'], $result['password_hash'])) {
+        throw new Exception("密码错误", 2);
+    }
+
+    # 将登陆信息写入会话
+    session_start();
+    $_SESSION['role_type'] = $result['role_type'];
+    $_SESSION['username'] = $user['username'];
+
+    $response = create_response();
+} catch (\PDOException $th) {
+    $response = create_response($th);
+} catch (Exception $th) {
+    $response = create_response($th);
+} finally {
+    $db = null;
+    echo json_encode($response);
+}
