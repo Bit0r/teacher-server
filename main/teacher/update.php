493bfc8c7fce880994b72875ac3ced2c5e3e5638
Bit0r <nie_wang@outlook.com>
2020-06-14 19:23:33 +0800

    🎨 修改更新功能代码，使之可修改任意列

--- main/teacher/update.php
+++ main/teacher/update.php
@@ -9,34 +9,30 @@ try {
     auth(ROLE_ADMIN);
     $db = connect_teacher();
 
-    # 构造查询语句
+    $teacher_id = $teacher['teacher_id'];
+    unset($teacher['teacher_id']);
+
+    # 构造查询语句和参数
     $query = 'UPDATE teacher SET';
-    if ($_SESSION['role_type'] == ROLE_ADMIN) {
-        $query .= empty($teacher['teacher_name']) ? '' : ' teacher_name = ?,';
-        $query .= empty($teacher['title']) ? '' : ' title = ?,';
-        $query .= empty($teacher['salary']) ? '' : ' salary = ?,';
-        $query .= empty($teacher['gender']) ? '' : ' gender = ?,';
+    if (isset($teacher['teacher_id_new'])) {
+        $teacher_id_new = $teacher['teacher_id_new'];
+        unset($teacher['teacher_id_new']);
+        $query .= ' teacher_id = ?,';
+        $params[] = $teacher_id_new;
     }
+
+    foreach ($teacher as $key => $value) {
+        $query .= " $key = ?,";
+        $params[] = $value;
+    }
+
     $query = mb_substr($query, 0, mb_strlen($query) - 1) . ' WHERE teacher_id = ?';
+    $params[] = $teacher_id;
 
+    #执行查询
     $stmt = $db->prepare($query);
-
-    # 构造查询参数
-    if (!empty($teacher['teacher_name'])) {
-        $params[] = $teacher['teacher_name'];
-    }
-    if (!empty($teacher['title'])) {
-        $params[] = $teacher['title'];
-    }
-    if (!empty($teacher['salary'])) {
-        $params[] = $teacher['salary'];
-    }
-    if (!empty($teacher['gender'])) {
-        $params[] = $teacher['gender'];
-    }
-    $params[] = $teacher['teacher_id'];
-
     $stmt->execute($params);
+
     $response = create_response();
 } catch (\PDOException $th) {
     $response = create_response($th);
