dfb2b4f91614d6f19a062f79d0c2e71ba7b9982c
Bit0r <nie_wang@outlook.com>
2020-06-15 16:28:03 +0800

    🐛 教师ID插入不正确

--- main/teacher/update.php
+++ main/teacher/update.php
@@ -10,17 +10,16 @@ try {
     $db = connect_teacher();
 
     $teacher_id = $teacher['teacher_id'];
-    unset($teacher['teacher_id']);
 
     # 构造查询语句和参数
     $query = 'UPDATE teacher SET';
     $teacher_id_new = $teacher['teacher_id_new'];
     if (!empty($teacher_id_new) && $teacher_id_new != $teacher_id) {
-        unset($teacher['teacher_id_new']);
         $query .= ' teacher_id = ?,';
         $params[] = $teacher_id_new;
     }
 
+    unset($teacher['teacher_id'], $teacher['teacher_id_new']);
     foreach ($teacher as $key => $value) {
         $query .= " $key = ?,";
         $params[] = $value;
